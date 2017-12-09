c483885d0bdd9aabba80f3147606a3cae90ced38
Mark Blanford <markytorture@hotmail.com>
2017-12-09 12:50:59 +0000

    on delete cascade problem fixed

--- console.rb
+++ console.rb
@@ -19,10 +19,10 @@ film2.save()
 
 ticket1 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film1.id})
 ticket1.save()
-# ticket2 = Ticket.new({'customer_id' => customer2.id, 'film_id' => film1.id})
-# ticket2.save()
-# ticket3 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film2.id})
-# ticket3.save()
+ticket2 = Ticket.new({'customer_id' => customer2.id, 'film_id' => film1.id})
+ticket2.save()
+ticket3 = Ticket.new({'customer_id' => customer1.id, 'film_id' => film2.id})
+ticket3.save()
 
 
 customer1.name = 'Mark E Blanford'
@@ -33,7 +33,6 @@ film1.title = 'Let Me In'
 film1.price = 6
 film1.update()
 
-ticket1.delete()
 
 customer_list = Customer.all()
 film_list = Film.all()
@@ -43,12 +42,14 @@ ticket_list = Ticket.all()
 
 customer2.delete()
 film2.delete()
+ticket1.delete()
 
 # customer1.remove_funds(8)
 # customer_funds = customer1.funds()
 
 updated_customer_list = Customer.all()
 updated_film_list = Film.all()
+updated_ticket_list = Ticket.all()
 
 binding.pry
 nil
--- db/cinema.sql
+++ db/cinema.sql
@@ -5,19 +5,17 @@ DROP TABLE IF EXISTS customers;
 CREATE TABLE customers (
   id SERIAL4 PRIMARY KEY,
   name VARCHAR(255),
-  funds INT2,
-  ON DELETE CASCADE
+  funds INT2
 );
 
 CREATE TABLE films (
   id SERIAL4 PRIMARY KEY,
   title VARCHAR(255),
-  price INT2,
-  ON DELETE CASCADE
+  price INT2
 );
 
 CREATE TABLE tickets (
   id SERIAL4 PRIMARY KEY,
-  customer_id INT4 REFERENCES customers(id),
-  film_id INT4 REFERENCES films(id)
+  customer_id INT4 REFERENCES customers(id) ON DELETE CASCADE,
+  film_id INT4 REFERENCES films(id) ON DELETE CASCADE
 );
